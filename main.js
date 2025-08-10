70241cfbb1591c7a045690333f01921013cc0436
Xakimxodjaev <f53660668@gmail.com>
2025-08-10 15:30:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt("ismingizni kiriting:");
+let id = prompt("ID raqamingizni kiriting:");
+let password = prompt("parolingizni kiriting:");
+
+console.log({name: name, id: id, password: password});
